c86be5901f3194da3863322333b2c1607d2958e1
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-02-18 21:46:32 -0600

    Add: Footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/aboutus" element={<Home />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+    return (
+        <footer className=" grid grid-cols-4 px-2 py-4">
+            <div className=" col-start-1 text-left font-bold text-2xl ">
+                IFGF HOUSTON
+            </div>
+            <div className=" col-start-1 text-left font-thin text-lg">
+                ALIVE IN CLARITY
+            </div>
+            <div className=" row-start-3 col-start-1 text-left font-thin text-base self-center">
+                Phone: +1 (281)922-4343
+            </div>
+            <div className=" row-start-3 col-start-2 text-left font-thin text-base">
+                <span className=" font-bold">International Full Gospel</span> <br/> 6455 Wilcrest Dr <br/> Houston, TX 77072
+            </div>
+            <div className=" row-start-3 col-start-4 text-right font-thin text-base self-center">
+                ©2023 by IFGF Houston
+            </div>
+            
+            <p></p>
+        </footer>
+    )
+}
+
+export default Footer;
