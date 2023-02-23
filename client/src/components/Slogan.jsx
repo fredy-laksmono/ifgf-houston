4cb0df8b42c5d31cfd195974302b99e4bc4e6c50
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-02-22 23:06:53 -0600

    Add: Responsive to slogan section

--- client/src/components/Slogan.jsx
+++ client/src/components/Slogan.jsx
@@ -1,15 +1,27 @@
 const Slogan = () => {
     return (
-        <section className=" bg-white text-black static grid grid-cols-1">
-            <div className=" people ">
-            <h1 className=" font-bold text-6xl ">PEOPLE IS</h1>
-            <h1 className=" font-bold text-6xl ">OUR MISSION</h1>
-            </div>
-            <div className=" connect ">
-            <h2 className=" font-semibold text-xl ">CONNECT WITH GOD</h2>
-            <h2 className=" font-semibold text-xl ">MAKE DISCIPLES</h2>
-            </div>
-        </section>
+        <div>
+            <section className=" bg-white text-black static flex flex-col md:hidden">
+                
+                <h1 className=" font-bold text-4xl pt-4">PEOPLE IS</h1>
+                <h1 className=" font-bold text-4xl ">OUR MISSION</h1>
+                
+                <h2 className=" font-semibold text-large pt-6 ">CONNECT WITH GOD</h2>
+                <h2 className=" font-semibold text-large pb-4">MAKE DISCIPLES</h2>
+                
+            </section>
+            <section className=" hidden bg-white text-black static md:grid grid-cols-1">
+                <div className=" people ">
+                <h1 className=" font-bold text-6xl ">PEOPLE IS</h1>
+                <h1 className=" font-bold text-6xl ">OUR MISSION</h1>
+                </div>
+                <div className=" connect ">
+                <h2 className=" font-semibold text-xl ">CONNECT WITH GOD</h2>
+                <h2 className=" font-semibold text-xl ">MAKE DISCIPLES</h2>
+                </div>
+            </section>
+        </div>
+        
     )
 }
 
