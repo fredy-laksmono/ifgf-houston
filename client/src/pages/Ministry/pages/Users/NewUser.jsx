ae45d09e91b9473b33bc3b16da289b4a4c5767be
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-02-25 19:24:15 -0600

    Update newUser section

--- client/src/pages/Ministry/pages/Users/NewUser.jsx
+++ client/src/pages/Ministry/pages/Users/NewUser.jsx
@@ -1,22 +1,36 @@
+import { useRef } from "react"
+
 const NewUser = ( {setNewUserMode}) => {
-    const submit = () => {
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const phoneRef = useRef()
+    const noteRef = useRef()
+
+    const onSubmit = () => {
         // Send user form to backend
+        let formData = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            phone: phoneRef.current.value,
+            note: noteRef.current.value
+        }
+        console.log(formData)
         setNewUserMode(false)
     }
     return (
         <div className=" grid grid-cols-7 py-4 bg-slate-200">
             <button className=" col-start-7 col-span-1 " onClick={()=>{setNewUserMode(false)}}>X</button>
             <div className=" row-start-2 col-start-1 col-span-6">
-                <form className=" grid grid-cols-7 gap-1">
+                <form onSubmit={onSubmit} className=" grid grid-cols-7 gap-1">
                     <label className=" col-start-1 col-span-1"> Name: </label>
-                    <input className=" col-start-2 col-span-6" type="text" />
+                    <input ref={nameRef} className=" col-start-2 col-span-6 px-2" type="text" />
                     <label className=" col-start-1 col-span-1"> Email: </label>
-                    <input className=" col-start-2 col-span-6" type="text" />
+                    <input ref={emailRef} className=" col-start-2 col-span-6 px-2" type="text" />
                     <label className=" col-start-1 col-span-1"> Phone: </label>
-                    <input className=" col-start-2 col-span-6" type="text" />
+                    <input ref={phoneRef} className=" col-start-2 col-span-6 px-2" type="text" />
                     <label className=" col-start-1 col-span-1"> Note: </label>
-                    <input className=" col-start-2 col-span-6" type="text" />
-                    <button className=" col-start-4 py-2 ">Submit</button>
+                    <input ref={noteRef} className=" col-start-2 col-span-6 px-2" type="text" />
+                    <button type="submit" className=" col-start-4 py-2 ">Submit</button>
                 </form>
             </div>
         </div>
