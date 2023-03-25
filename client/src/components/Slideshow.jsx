ec08a25a47bd525cc02874ed0dd4b25ff19278e9
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-03-24 22:12:51 -0500

    Add responsiveness to Welcome Home section

--- client/src/components/Slideshow.jsx
+++ client/src/components/Slideshow.jsx
@@ -14,21 +14,21 @@ const Slideshow = ({ images, interval = 5000 }) => {
   const welcomeHomeTitle = (
 
     <div class="absolute inset-0 flex items-center justify-center">
-    <div class="px-4 py-2 bg-gray-900 bg-opacity-25 text-white rounded-md">
-      <h2 class="text-center font-bold text-3xl">WELCOME HOME</h2>
+    <div class="px-2 py-2 bg-gray-900 bg-opacity-25 text-white rounded-lg">
+      <h2 class="text-center font-bold text-3xl sm:text-5xl md:text-7xl lg:text-9xl">WELCOME HOME</h2>
     </div>
   </div>
   )
 
   return (
-    <div className="relative h-52 overflow-hidden">
+    <div className="relative h-48 overflow-hidden sm:h-72 md:h-96 lg:h-screen">
       {images.map((image, index) => (
         <div>
             <img
                 key={index}
                 src={image}
                 alt={`Slideshow image ${index}`}
-                className={`absolute top-0 left-0 w-full transition-opacity duration-1000 ${
+                className={`absolute top-0 left-0 w-full h-48 sm:h-72 md:h-96 lg:h-screen transition-opacity duration-1000 ${
                     index === currentSlide ? 'opacity-100' : 'opacity-0'
                 }`}
             />
