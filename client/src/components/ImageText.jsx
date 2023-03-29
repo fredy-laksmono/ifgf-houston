8ea8e56378b8adaaf320f3bef5818814f5202e52
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-03-28 20:20:25 -0500

    Add Vision page content

--- client/src/components/ImageText.jsx
+++ client/src/components/ImageText.jsx
@@ -1,13 +1,13 @@
 const ImageText = ({ imgPath, imageAlt = "Image", text }) => {
     return (
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div className="  flex justify-center items-center max-w-screen-sm">
+        <div className="  flex justify-center items-center ">
             <img src={imgPath} className=" justify-center items-center md:h-full" alt={imageAlt} />
         </div>
         
-        <div className="flex flex-col justify-center max-w-screen-sm">
-          <h2 className="text-2xl font-bold mb-4">{text.title}</h2>
-          <p className="text-lg text-left">{text.description}</p>
+        <div className="flex flex-col justify-center">
+          <h2 className="text-2xl md:text-xl font-bold mb-4">{text.title}</h2>
+          <p className="text-lg md:text-base text-left">{text.description}</p>
         </div>
       </div>
     );
--- client/src/pages/Vision/Vision.js
+++ client/src/pages/Vision/Vision.js
@@ -1,6 +1,14 @@
 import ImageText from "../../components/ImageText";
 import prayerImage from "../../assets/vision/HouseOfPrayer.jpeg";
 import powerImage from "../../assets/vision/HouseOfPower.jpeg";
+import worshipImage from "../../assets/vision/HouseOfWorship.jpeg";
+import covenantImage from "../../assets/vision/HouseOfCovenant.jpeg";
+import wordImage from "../../assets/vision/HouseOfWord.jpeg";
+import missionImage from "../../assets/vision/HouseOfMission.jpeg";
+import balanceImage from "../../assets/vision/HouseOfBalance.jpeg";
+import givingImage from "../../assets/vision/HouseOfGiving.jpeg";
+import excellenceImage from "../../assets/vision/HouseOfExcellence.jpeg";
+import leadershipImage from "../../assets/vision/HouseOfLeadership.jpeg";
 
 const Vision = () => {
   const prayer = {
@@ -24,12 +32,109 @@ const Vision = () => {
     )
   };
 
+  const worship = {
+    title: "House of Worship",
+    description: (
+      <p>
+        To build individuals, leaders, and church that like to praise and
+        worship. <br /> <br />
+        Matthew 21:15-16; John 4:23-24; Acts 2:47, 16:15-34; Psalms 149:6-9
+      </p>
+    )
+  };
+
+  const covenant = {
+    title: "House of Covenant",
+    description: (
+      <p>
+        To build individuals, leaders, and church that are united in covenant.
+        <br /> <br />
+        Matthew 13:47; John 17:20-23; Acts 2:44,46; Ephesians 2:21-22; Psalms
+        133; Ecclesiastes 4:9-12
+      </p>
+    )
+  };
+
+  const word = {
+    title: "House of Word",
+    description: (
+      <p>
+        To build individuals, leaders, and church that are rooted in the Word.
+        <br /> <br />
+        John 8:32; Acts 2:42, 17:10-11; Ephesians 4:11-15; Isaiah 2:2-3
+      </p>
+    )
+  };
+
+  const mission = {
+    title: "House of Covenant",
+    description: (
+      <p>
+        To build individuals, leaders, and church that have world mission.
+        <br /> <br />
+        Matthew 28:19-20; John 3:16; Acts 1:8; 1 John 5:1; Psalms 2:8
+      </p>
+    )
+  };
+
+  const balance = {
+    title: "House of Covenant",
+    description: (
+      <p>
+        To build individuals, leaders, and church that are balanced.
+        <br /> <br />
+        Proverbs 11:1, 16:11, 20:23
+      </p>
+    )
+  };
+
+  const giving = {
+    title: "House of Covenant",
+    description: (
+      <p>
+        To build individuals, leaders, and church that give.
+        <br /> <br />
+        Acts 2:44-45, 4:34-35; 2 Corinthians 8:1-12, 9:6-8; Malachi 3:10
+      </p>
+    )
+  };
+
+  const excellence = {
+    title: "House of Excellence",
+    description: (
+      <p>
+        To build individuals, leaders, and church that are excellent.
+        <br /> <br />
+        Ephesians 5:27; 2 Kings 10:1-19; Haggai 2:9
+      </p>
+    )
+  };
+
+  const leadership = {
+    title: "House of Godly and Skillful Leadership",
+    description: (
+      <p>
+        To build individuals, leaders, and church that are godly and skillful.
+        <br /> <br />
+        Acts 6:3; 1 Timothy 3:1-7; Titus 1:5-9; Hebrews 3:1-6; Exodus 18:21
+      </p>
+    )
+  };
+
   return (
     <div className=" py-4 px-8 bg-white text-black">
       <h1 className=" text-3xl mb-5">Our Vision</h1>
       <div className=" grid grid-cols-1 md:grid-cols-2 gap-4">
         <ImageText imgPath={prayerImage} text={prayer} />
         <ImageText imgPath={powerImage} text={power} />
+        <ImageText imgPath={worshipImage} text={worship} />
+        <ImageText imgPath={covenantImage} text={covenant} />
+        <ImageText imgPath={wordImage} text={word} />
+        <ImageText imgPath={missionImage} text={mission} />
+        <ImageText imgPath={balanceImage} text={balance} />
+        <ImageText imgPath={givingImage} text={giving} />
+        <ImageText imgPath={excellenceImage} text={excellence} />
+        <ImageText imgPath={leadershipImage} text={leadership} />
       </div>
     </div>
   );
