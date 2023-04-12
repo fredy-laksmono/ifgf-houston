701fc54c6cf719ae156df8e73c5cce087b1c0c9b
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-04-11 19:03:54 -0500

    Initialize Instagram Feed feature

---
+++ client/src/pages/Home/InstagramFeed.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+function InstagramFeed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${process.env.REACT_APP_INSTAGRAM_ACCESS_TOKEN}&limit=7`);
+      const data = await response.json();
+      setPosts(data.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>
+          {post.media_type === "VIDEO" ? (
+            <video src={post.media_url} alt={post.caption} />
+          ) : (
+            <img src={post.media_url} alt={post.caption} />
+          )}
+          <p>{post.caption}</p>
+          <a href={post.permalink}>View on Instagram</a>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default InstagramFeed;
