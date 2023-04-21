ad101aeb46ace206e2aaab85d1b5513e5bca92e4
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-04-20 21:38:22 -0500

    Update placholder link on ministry sidebar

--- client/src/pages/Ministry/components/SideBar.jsx
+++ client/src/pages/Ministry/components/SideBar.jsx
@@ -4,13 +4,13 @@ const SideBar = () => {
     return (
         <div>
             <div className=" flex flex-col h-auto py-4 items-center" aria-label="SideBar">
-                <Link className=' w-full text-center py-1 hover:opacity-90' to="/ministry" style={{textDecoration: 'none'}}>Ministry Schedule</Link>
+                <Link className='w-full text-center py-1 hover:opacity-90' to="/ministry" style={{textDecoration: 'none'}}>Ministry Schedule</Link>
                 <Link className='w-full text-center py-1 hover:opacity-90' to="/ministry" style={{textDecoration: 'none'}}>My Schedule</Link>
-                <Link className='w-full text-center py-1 hover:opacity-90' to="/" style={{textDecoration: 'none'}}>My Availability</Link>
-                <Link className='w-full text-center py-1 hover:opacity-90' to="/" style={{textDecoration: 'none'}}>Manage Schedules</Link>
+                <Link className='w-full text-center py-1 hover:opacity-90' to="/ministry" style={{textDecoration: 'none'}}>My Availability</Link>
+                <Link className='w-full text-center py-1 hover:opacity-90' to="/ministry" style={{textDecoration: 'none'}}>Manage Schedules</Link>
                 <Link className='w-full text-center py-1 hover:opacity-90' to="/ministry/users" style={{textDecoration: 'none'}}>Users</Link>
-                <Link className='w-full text-center py-1 hover:opacity-90' to="/" style={{textDecoration: 'none'}}>Roles</Link>
-                <Link className='w-full text-center py-1 hover:opacity-90' to="/" style={{textDecoration: 'none'}}>Events</Link>
+                <Link className='w-full text-center py-1 hover:opacity-90' to="/ministry" style={{textDecoration: 'none'}}>Roles</Link>
+                <Link className='w-full text-center py-1 hover:opacity-90' to="/ministry" style={{textDecoration: 'none'}}>Events</Link>
             </div>
         </div>
     )
