e0bcc4533c8934757a057bd63fca293850346ec5
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-04-27 20:06:48 -0500

    Update market link

--- client/src/pages/Home/Event.jsx
+++ client/src/pages/Home/Event.jsx
@@ -1,18 +1,25 @@
-import bkkihEasterImage from '../../assets/BKKIHPassOver2032.jpeg'
 import InfoPanel from "./InfoPanel";
+import { useNavigate } from "react-router-dom";
 
 const Event = () => {
-    const bkkihEasterService = (
-        <p className=' pt-4'>
-            <a href="https://bit.ly/easter_bkkih" className="bg-white hover:bg-blue-500 text-black font-bold py-2 px-4 rounded" target="_blank" rel="noreferrer">
-                RSVP Here
-            </a>
-        </p>
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/indomarket');
+      };
+
+    const indoMarket = (
+        // <p className=' pt-4'>
+        //     <a href="https://bit.ly/easter_bkkih" className="bg-white hover:bg-blue-500 text-black font-bold py-2 px-4 rounded" target="_blank" rel="noreferrer">
+        //         RSVP Here
+        //     </a>
+        // </p>
+        <button className=" text-lg bg-white hover:bg-blue-500 text-black font-bold py-2 px-4 rounded" onClick={handleClick}>View Menu</button>
     )
     return (
         <div className=" bg-black text-white pb-4 ">
             <h1 className=" font-bold text-4xl pt-8">UPCOMING SPECIAL EVENTS</h1>
-            <InfoPanel imagePath="https://gcdnb.pbrd.co/images/mSebwAzoWt3h.jpg?o=1" imageAlt="IFGF Houston Bazzar May 6 2023 Promo Image" title="IFGF INDO MARKET" />
+            <InfoPanel imagePath="https://gcdnb.pbrd.co/images/mSebwAzoWt3h.jpg?o=1" imageAlt="IFGF Houston Bazzar May 6 2023 Promo Image" title="IFGF INDO MARKET" body={indoMarket}/>
         </div>
     )
 }
