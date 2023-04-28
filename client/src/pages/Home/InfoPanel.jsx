3ce7c602af5ba75a079ba92b3e237354202155cf
Fredy Laksmono <fredy.laksmono@gmail.com>
2023-04-27 20:10:09 -0500

    Update infopanel title to top

--- client/src/pages/Home/InfoPanel.jsx
+++ client/src/pages/Home/InfoPanel.jsx
@@ -5,8 +5,8 @@ const InfoPanel = ({imagePath, imageAlt, title, body, style}) => {
         case "white":
             toRender = (
                 <div className=" py-3 grid place-items-center gap-2 bg-white text-black ">
-                    {imagePath ? <img className="  bg-cover bg-center p-8 box-border" src={imagePath} alt={imageAlt}/> : null}
-                    <h2 className=" text-2xl font-bold">{title}</h2>
+                    <h2 className=" pt-4 text-2xl font-bold">{title}</h2>
+                    {imagePath ? <img className="  bg-cover bg-center px-8 pt-4 pb-2 box-border" src={imagePath} alt={imageAlt}/> : null}
                     {body}
                 </div>
             );
@@ -14,8 +14,8 @@ const InfoPanel = ({imagePath, imageAlt, title, body, style}) => {
         default:
             toRender = (
                 <div className=" py-3 grid place-items-center gap-2 bg-black text-white ">
-                    {imagePath ? <img className="  bg-cover bg-center p-8 box-border" src={imagePath} alt={imageAlt}/> : null}
-                    <h2 className=" text-2xl font-bold">{title}</h2>
+                    <h2 className=" pt-4 text-2xl font-bold">{title}</h2>
+                    {imagePath ? <img className="  bg-cover bg-center px-8 pt-4 pb-2 box-border" src={imagePath} alt={imageAlt}/> : null}
                     {body}
                 </div>
             );
